9c246bf5cbd9217623677fd6f1669ea69392a2af
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-11-04 18:06:09 +0100

    Added homepage image

--- src/Homepage/Home.jsx
+++ src/Homepage/Home.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
+import homepageImg from "../assets/HomepageImage.png";
 import NavHeader from "../NavHeader/NavHeader";
+import classes from './home.module.css';
 
 export default function Homepage() {
 
     return (
         <div>
             <NavHeader />
+            <img src={homepageImg} alt="" />
             <p>test homepage</p>
         </div>
     )
